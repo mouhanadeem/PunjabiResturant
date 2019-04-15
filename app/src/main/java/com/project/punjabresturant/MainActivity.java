fc658717fd17c5b0a321b5f4bff7b181d606a774
mouhanadeem <nadeem4eco@yahoo.com>
2019-04-15 22:33:09 +0200

    layout

--- app/src/main/java/com/project/punjabresturant/MainActivity.java
+++ app/src/main/java/com/project/punjabresturant/MainActivity.java
@@ -2,12 +2,31 @@ package com.project.punjabresturant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button bthSignIn,bthSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bthSignIn=findViewById(R.id.btnSignIn);
+        bthSignUp=findViewById(R.id.btnSignUP);
+        bthSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
+        bthSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
